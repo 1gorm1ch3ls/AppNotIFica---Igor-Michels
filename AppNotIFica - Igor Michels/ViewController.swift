936a791a61021970a93431bdc9c43432f6d8e73f
1gorm1ch3ls <igormichels06@gmail.com>
2023-06-12 09:04:44 -0300

    Initial Commit

---
+++ AppNotIFica - Igor Michels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotIFica - Igor Michels
+//
+//  Created by IFBIOTIC06 on 12/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
